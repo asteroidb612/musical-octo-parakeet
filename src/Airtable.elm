23f03f815fee9ac4db5574310125657e5d5d5527
asteroidb612 <dlazzeri1@gmail.com>
2022-06-11 14:49:16 +0000

    Fetching from AirTable

---
+++ src/Airtable.elm
@@ -0,0 +1,36 @@
+module Airtable exposing (getRecords)
+
+{-| 
+    Based on https://github.com/altjsus/elmtable. Intended as a read-only GET/HTTPS based Airtable-as-CMS. Only reads multiple records
+    @docs getRecords
+-}
+
+import Http
+import Url.Builder
+import Http exposing (..)
+
+type alias DB = {apiKey : String, base : String, table : String}
+
+{-| 
+    GET a list of records from an Airtable Table, by passing it a Base, View, and an Http.ExpectJSON
+-}
+--getRecords db dbView maxRecords pageSize offset expect =
+getRecords: DB -> String -> Expect msg -> Cmd msg
+getRecords db dbView expect =
+    Http.request {
+        method = "GET"
+        , headers = [Http.header "Authorization" ("Bearer " ++ db.apiKey)]
+        , url = "https://api.airtable.com/v0" ++ 
+            (Url.Builder.absolute [db.base, db.table] 
+                [
+                    Url.Builder.string "view" dbView
+                    --Url.Builder.int "maxRecords" maxRecords, 
+                    --Url.Builder.int "pageSize" pageSize, 
+                    --Url.Builder.int "offset" offset
+                ]
+            )
+        , expect = expect
+        , body = Http.emptyBody
+        , timeout = Nothing
+        , tracker = Nothing
+    }
--- src/Main.elm
+++ src/Main.elm
@@ -1,47 +1,164 @@
 module Main exposing (main)
 
+{-| 
+    Simple Elm wrapper around the Airtable API.
+    @docs main 
+-}
+
+
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html)
+import Html.Attributes
+
+---
+
+import Http
+import Json.Decode as Decode exposing (Decoder, string)
+import Json.Decode.Pipeline exposing (requiredAt)
+import Element exposing (..)
+
+---
+
+import Airtable
 
 
-type alias Model =
-    { count : Int }
+--- TYPES
+
+type alias DB = {apiKey : String, base : String, table : String}
+
+{-| 
+    Use Environment Variables, DO NOT pass your API keys and other secrets this way. These values are for a dummy Airtable created expressly for this purpose.
+    
+-}
 
 
-initialModel : Model
-initialModel =
-    { count = 0 }
+myDB = {apiKey = "keyWeDr2vJl8zd3mG", base = "appFMiCjufAGkO5X4", table = "Examples"}   
 
+
+type Model
+  = Failure
+  | Loading
+  | Success (List Record)
 
 type Msg
-    = Increment
-    | Decrement
+  = GotRecords(Result Http.Error (List Record))
+
+type alias Record = 
+    { name : String
+    , job: String
+    }
 
 
-update : Msg -> Model -> Model
+--- JSON DECODERS
+
+{-| 
+    Airtable nests records two levels deep; an outer object titled "records", and inner elements titled "fields". This is why decoding is done in two stages, for each record, and then, for a list of records. In hindsight, "Record" was perhaps not the most ideal name for a type chosen, but YMMV.
+    
+-}
+
+
+recordDecoder: Decoder Record
+recordDecoder = 
+    Decode.succeed Record
+        |> requiredAt [ "fields", "Name"] Decode.string
+        |> requiredAt [ "fields", "Job"] Decode.string
+
+recordsDecoder : Decoder (List Record)
+recordsDecoder =
+  Decode.field "records" (Decode.list recordDecoder)
+
+
+--- INIT
+
+{-| 
+    Here, we are passing an Airtable View ("Main" in our case). If you don;t do so, records are randomly ordered. Naming a View allows the end user to sort as they wish using Airtable's simple and excellent UI.
+    
+-}
+
+--- 
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Loading
+
+  --, Airtable.getRecords myDB "Main" 100 100 0 (Http.expectJson GotRecords recordsDecoder)
+  , Airtable.getRecords myDB "Main" (Http.expectJson GotRecords recordsDecoder)
+
+  )
+
+--- UPDATE
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    case msg of
-        Increment ->
-            { model | count = model.count + 1 }
+  case msg of
+    GotRecords result ->
+      case result of
+        Ok records ->
+          (Success records, Cmd.none)
 
-        Decrement ->
-            { model | count = model.count - 1 }
+        Err _ ->
+          (Failure, Cmd.none)
+
+
+--- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+--- VIEWS
+
+{-| 
+    The final view is a bare-bones MVP-style display of Airtable data. Styling is left upto the reader.
+    
+-}
+
+recordView : List Record -> Element Msg
+recordView records = 
+    column []
+        [ Element.html (
+            Html.ul [] 
+                (List.map 
+                    (\x -> (
+                        Html.li [] [ Html.text (x.job ++ " " ++ x.name) ]
+                    )
+                ) records )
+            )
+        ]            
+
+
+airtableView : List Record -> Html Msg
+airtableView records = 
+    layout [] <| column [][ recordView records ]
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Increment ] [ text "+1" ]
-        , div [] [ text <| String.fromInt model.count ]
-        , button [ onClick Decrement ] [ text "-1" ]
-        ]
+  case model of
+    Failure -> 
+      Html.text "Something went wrong"
+
+    Loading ->
+      Html.text "elm-airtable loading"
+
+    Success records ->
+        airtableView records
 
 
-main : Program () Model Msg
+--- MAIN
+
+{-| 
+    main
+     
+-}
+main: Program () Model Msg
 main =
-    Browser.sandbox
-        { init = initialModel
-        , view = view
-        , update = update
-        }
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
